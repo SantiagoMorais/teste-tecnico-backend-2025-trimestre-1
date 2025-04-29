bc65129db745363c5ff60b8249fb86a9384793ae
Felipe <fillikz@gmail.com>
2025-04-29 16:05:29 -0300

    feat(server): integrate video routes

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import videoRoutes from "@/http/routes/video-routes";
 
 export const app = express();
 const PORT = 3000;
@@ -6,6 +7,9 @@ const PORT = 3000;
 app.get("/", (_, res) => {
   res.status(200).json({ message: "It's working" });
 });
+
+app.use(express.json());
+app.use(videoRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
